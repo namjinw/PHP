b2409c7cdeead2cffba6360c9d52a1d069f9a335
namjinw <0812jinwoo@gmail.com>
2025-09-02 17:44:28 +0900

    Add files via upload

---
+++ php_html2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>로그인 확인</h1>
+    <form method="post">
+        <input type="text" name="userid" required><br>
+        <input type="text" name="usertag" required><br>
+        <input type="submit" value="로그인">
+    </form>
+    <?php
+    if (isset($_POST["userid"])) { 
+        $userid = $_POST["userid"];
+        $usertag= $_POST["usertag"];
+    ?>
+        <div class="login-box">
+            <h1>환엽합니다! <?php echo $userid ?>님</h1>
+            <p><?php echo $usertag ?>이시군요!</p>
+        </div>
+<?php }
+    else { ?>
+    <div class="register-box">
+        <h1>유저 정보를 입력하시오</h1>
+    </div>
+    <?php } ?>
+</body>
+</html>
